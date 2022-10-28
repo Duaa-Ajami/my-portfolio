734313b28db21790cf7d0b571eed402bbf66ba7a
Duaa <97605696+Duaa-Ajami@users.noreply.github.com>
2022-10-29 01:28:09 +0200

    tow commit

---
+++ Map/demo/b/assets/glob/mapdata.js
@@ -0,0 +1,610 @@
+// var simplemaps_europemap_mapdata={
+//   main_settings: {
+//    //General settings
+//     width: "responsive", //'700' or 'responsive'
+//     background_color: "#FFFFFF",
+//     background_transparent: "yes",
+//     border_color: "#ffffff",
+//     popups: "detect",
+
+//     //State defaults
+//     state_description: "",
+//     state_color: "#397051",
+//     state_hover_color: "#04aa6d",
+//     state_url: "",
+//     border_size: 1.5,
+//     all_states_inactive: "no",
+//     all_states_zoomable: "yes",
+//     location_description: "",
+//     location_color: "#04aa6d",
+//     location_opacity: 0.8,
+//     location_hover_opacity: 1,
+//     location_url: "",
+//     location_size: 25,
+//     location_type: "circle",
+//     location_image_source: "frog.png",
+//     location_border_color: "#FFFFFF",
+//     location_border: 2,
+//     location_hover_border: 2.5,
+//     all_locations_inactive: "no",
+//     all_locations_hidden: "no",
+
+//     //Label defaults
+//     label_color: "#d5ddec",
+//     label_hover_color: "#d5ddec",
+//     label_size: 22,
+//     label_font: "Arial",
+//     hide_labels: "no",
+//     manual_zoom: "no",
+//     back_image: "no",
+//     arrow_color: "#cecece",
+//     arrow_color_border: "#808080",
+//     initial_back: "no",
+//     initial_zoom: -1,
+//     initial_zoom_solo: "no",
+//     region_opacity: 1,
+//     region_hover_opacity: 0.6,
+//     zoom_out_incrementally: "yes",
+//     zoom_percentage: 0.99,
+//     zoom_time: 0.5,
+
+//     //Popup settings
+//     popup_color: "white",
+//     popup_opacity: 0.9,
+//     popup_shadow: 1,
+//     popup_corners: 5,
+//     popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+//     popup_nocss: "no",
+
+//     //Advanced settings
+//     div: "map",
+//     auto_load: "yes",
+//     url_new_tab: "yes",
+//     images_directory: "default",
+//     fade_time: 0.1,
+//     link_text: "View Website",
+//     state_image_url: "",
+//     state_image_position: "",
+//     location_image_url: ""
+//   },
+//   state_specific: {
+//     AL: {
+//       name: "Albania"
+//     },
+//     AM: {
+//       name: "Armenia"
+//     },
+//     AT: {
+//       name: "Austria"
+//     },
+//     BA: {
+//       name: "Bosnia and Herzegovina"
+//     },
+//     BE: {
+//       name: "Belgium"
+//     },
+//     BG: {
+//       name: "Bulgaria"
+//     },
+//     BY: {
+//       name: "Belarus"
+//     },
+//     CH: {
+//       name: "Switzerland"
+//     },
+//     CY: {
+//       name: "Cyprus"
+//     },
+//     CZ: {
+//       name: "Czech Republic"
+//     },
+//     DE: {
+//       name: "Germany"
+//     },
+//     DK: {
+//       name: "Denmark"
+//     },
+//     EE: {
+//       name: "Estonia"
+//     },
+//     ES: {
+//       name: "Spain"
+//     },
+//     FI: {
+//       name: "Finland"
+//     },
+//     FR: {
+//       name: "France"
+//     },
+//     GB: {
+//       name: "United Kingdom"
+//     },
+//     GE: {
+//       name: "Georgia"
+//     },
+//     GR: {
+//       name: "Greece"
+//     },
+//     HR: {
+//       name: "Croatia"
+//     },
+//     HU: {
+//       name: "Hungary"
+//     },
+//     IE: {
+//       name: "Ireland"
+//     },
+//     IS: {
+//       name: "Iceland"
+//     },
+//     IT: {
+//       name: "Italy"
+//     },
+//     LT: {
+//       name: "Lithuania"
+//     },
+//     LU: {
+//       name: "Luxembourg"
+//     },
+//     LV: {
+//       name: "Latvia"
+//     },
+//     MD: {
+//       name: "Moldova"
+//     },
+//     ME: {
+//       name: "Montenegro"
+//     },
+//     MK: {
+//       name: "Macedonia"
+//     },
+//     NL: {
+//       name: "Netherlands"
+//     },
+//     NO: {
+//       name: "Norway"
+//     },
+//     PL: {
+//       name: "Poland"
+//     },
+//     PT: {
+//       name: "Portugal"
+//     },
+//     RO: {
+//       name: "Romania"
+//     },
+//     RS: {
+//       name: "Serbia"
+//     },
+//     SE: {
+//       name: "Sweden"
+//     },
+//     SI: {
+//       name: "Slovenia"
+//     },
+//     SK: {
+//       name: "Slovakia"
+//     },
+//     TR: {
+//       name: "Turkey"
+//     },
+//     UA: {
+//       name: "Ukraine"
+//     }
+//   },
+//   locations: {
+//     "0": {
+//       lat: 48.857,
+//       lng: 2.341,
+//       name: "Paris",
+//       url:'https://vega-sy.com/halal/locale1/fr'
+//     },
+//     "1": {
+//       lat: "51.509",
+//       lng: "-0.118",
+//       name: "UK",
+//       url:'https://vega-sy.com/halal/locale1/en'
+//     },
+//     "2": {
+//       lat: "52.531",
+//       lng: "13.381",
+//       name: "Germany",
+//       url:'https://vega-sy.com/halal/locale1/de'
+//     },
+//     "3": {
+//       lat: "52.370",
+//       lng: "4.895",
+//       name: "Netherlands",
+//       url:'https://vega-sy.com/halal/locale1/de'
+//     },
+//     "4": {
+//       lat: "50.847",
+//       lng: "4.357",
+//       name: "Belgium",
+//       url:'https://vega-sy.com/halal/locale1/be'
+//     },
+//     "5": {
+//       lat: "52.229",
+//       lng: "21.012",
+//       name: "Poland",
+//       url:'https://vega-sy.com/halal/locale1/pl'
+//     },
+//     "6": {
+//       lat: "41.902",
+//       lng: "12.496",
+//       name: "Italy",
+//       url:'https://vega-sy.com/halal/locale1/en',
+//     },
+//     "7": {
+//       lat: "53.349",
+//       lng: "-6.260",
+//       name: "Ireland",
+//       url:'https://vega-sy.com/halal/locale1/en'
+//     },
+//     "8": {
+//       lat: "48.208",
+//       lng: "16.373",
+//       name: "Austria",
+//       url:'https://vega-sy.com/halal/locale1/en'
+//     },
+//     "9": {
+//       lat: "55.676",
+//       lng: "12.568",
+//       name: "Denmark",
+//       url:'https://vega-sy.com/halal/locale1/dk'
+//     },
+//     "10": {
+//       lat: "59.329",
+//       lng: "18.068",
+//       name: "Sweden",
+//       url:'https://vega-sy.com/halal/locale1/en'
+//     },
+//     "11": {
+//       lat: "59.913",
+//       lng: "10.752",
+//       name: "Norway",
+//       url:'https://vega-sy.com/halal/locale1/en'
+//     }
+//   },
+//   regions: {},
+//   labels: {},
+//   legend: {
+//     entries: []
+//   }
+// };
+var simplemaps_europemap_mapdata = {
+    main_settings: {
+        //General settings
+        width: "responsive", //'700' or 'responsive'
+        background_color: "#04aa6d",
+        background_transparent: "yes",
+        border_color: "",
+        popups: "detect",
+
+        //State defaults
+        state_description: "State description",
+        state_color: "#397051",
+        state_hover_color: "#04aa6d",
+        state_url: "",
+        border_size: 1.5,
+        all_states_inactive: "no",
+        all_states_zoomable: "no",
+        location_description: "",
+        location_color: "#e2e7ec",
+        location_opacity: "0",
+        location_hover_opacity: 1,
+        location_url: "",
+        location_size: "30",
+        location_type: "circle",
+        location_image_source: "frog.png",
+        location_border_color: "#FFFFFF",
+        location_border: 2,
+        location_hover_border: 2.5,
+        all_locations_inactive: "no",
+        all_locations_hidden: "no",
+
+        //Label defaults
+        label_color: "#88d96f",
+        label_hover_color: "#d5ddec",
+        label_size: "24",
+        label_hover_size: "50",
+        label_font: "Arial",
+        hide_labels: "no",
+        manual_zoom: "no",
+        back_image: "no",
+        arrow_color: "#cecece",
+        arrow_color_border: "#808080",
+        initial_back: "no",
+        initial_zoom: "1",
+        initial_zoom_solo: "no",
+        region_opacity: 1,
+        region_hover_opacity: 0.6,
+        zoom_out_incrementally: "yes",
+        zoom_percentage: 0.99,
+        zoom_time: 0.5,
+
+        //Popup settings
+        popup_color: "white",
+        popup_opacity: 0.9,
+        popup_shadow: 1,
+        popup_corners: 5,
+        popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+        popup_nocss: "no",
+
+        //Advanced settings
+        div: "map",
+        auto_load: "yes",
+        url_new_tab: "yes",
+        images_directory: "default",
+        fade_time: 0.1,
+        link_text: "View Website",
+        state_image_url: "",
+        state_image_position: "",
+        location_image_url: "",
+        border_hover_color: "#04aa6d"
+    },
+    state_specific: {
+        AL: {
+            name: "Albania"
+        },
+        AM: {
+            name: "Armenia"
+        },
+        AT: {
+            name: "Austria"
+        },
+        BA: {
+            name: "Bosnia and Herzegovina"
+        },
+        BE: {
+            name: "Belgium"
+        },
+        BG: {
+            name: "Bulgaria"
+        },
+        BY: {
+            name: "Belarus"
+        },
+        CH: {
+            name: "Switzerland"
+        },
+        CY: {
+            name: "Cyprus"
+        },
+        CZ: {
+            name: "Czech Republic"
+        },
+        DE: {
+            name: "Germany"
+        },
+        DK: {
+            name: "Denmark"
+        },
+        EE: {
+            name: "Estonia"
+        },
+        ES: {
+            name: "Spain"
+        },
+        FI: {
+            name: "Finland"
+        },
+        FR: {
+            name: "France"
+        },
+        GB: {
+            name: "United Kingdom"
+        },
+        GE: {
+            name: "Georgia"
+        },
+        GR: {
+            name: "Greece"
+        },
+        HR: {
+            name: "Croatia"
+        },
+        HU: {
+            name: "Hungary"
+        },
+        IE: {
+            name: "Ireland"
+        },
+        IS: {
+            name: "Iceland"
+        },
+        IT: {
+            name: "Italy"
+        },
+        LT: {
+            name: "Lithuania"
+        },
+        LU: {
+            name: "Luxembourg"
+        },
+        LV: {
+            name: "Latvia"
+        },
+        MD: {
+            name: "Moldova"
+        },
+        ME: {
+            name: "Montenegro"
+        },
+        MK: {
+            name: "Macedonia"
+        },
+        NL: {
+            name: "Netherlands"
+        },
+        NO: {
+            name: "Norway"
+        },
+        PL: {
+            name: "Poland"
+        },
+        PT: {
+            name: "Portugal"
+        },
+        RO: {
+            name: "Romania"
+        },
+        RS: {
+            name: "Serbia"
+        },
+        SE: {
+            name: "Sweden"
+        },
+        SI: {
+            name: "Slovenia"
+        },
+        SK: {
+            name: "Slovakia"
+        },
+        TR: {
+            name: "Turkey"
+        },
+        UA: {
+            name: "Ukraine"
+        }
+    },
+    locations: {
+        "0": {
+            lat: "46.227",
+            lng: "2.213",
+            name: "Paris",
+            url: "http://vega-sy.com/halal/locale/fr"
+        },
+        "1": {
+            lat: "51.509",
+            lng: "-0.118",
+            name: "London"
+        },
+        "2": {
+            lat: "52.531",
+            lng: "13.381",
+            name: "Berlin"
+        },
+        "3": {
+            lat: "52.370",
+            lng: "4.895",
+            name: "Amsterdam"
+        },
+        "4": {
+            lat: "50.847",
+            lng: "4.357",
+            name: "Brussels"
+        },
+        "5": {
+            lat: "52.229",
+            lng: "21.012",
+            name: "Warsaw"
+        },
+        "6": {
+            lat: "41.902",
+            lng: "12.496",
+            name: "Rome"
+        },
+        "7": {
+            lat: "53.349",
+            lng: "-6.260",
+            name: "Dublin"
+        },
+        "8": {
+            lat: "48.208",
+            lng: "16.373",
+            name: "Vienna"
+        },
+        "9": {
+            lat: "55.676",
+            lng: "12.568",
+            name: "Copenhagen"
+        },
+        "10": {
+            lat: "59.329",
+            lng: "18.068",
+            name: "Stockholm"
+        },
+        "11": {
+            lat: "59.913",
+            lng: "10.752",
+            name: "Oslo"
+        }
+    },
+    regions: {},
+    labels: {
+        "0": {
+            name: "UK",
+            parent_id: "GB",
+            x: "376.871",
+            y: "342.912"
+        },
+        "1": {
+            name: "Ireland",
+            x: 293.5831,
+            y: 382.365,
+            parent_id: "IE"
+        },
+        "2": {
+            name: "Norway",
+            x: 464.54431,
+            y: 239.89733,
+            parent_id: "NO"
+        },
+        "3": {
+            name: "Sweden",
+            x: 593.86112,
+            y: 156.60853,
+            parent_id: "SE"
+        },
+        "4": {
+            name: "Poland",
+            x: 626.73828,
+            y: 347.29603,
+            parent_id: "PL"
+        },
+        "5": {
+            name: "Italy",
+            x: 585.09388,
+            y: 605.92966,
+            parent_id: "IT"
+        },
+        "6": {
+            name: "France",
+            x: 427.28353,
+            y: 500.72276,
+            parent_id: "FR"
+        },
+        "7": {
+            name: "Spain",
+            x: 352.76198,
+            y: 597.16241,
+            parent_id: "ES"
+        },
+        "8": {
+            name: "Denmark",
+            x: 508.38052,
+            y: 312.22707,
+            parent_id: "DK"
+        },
+        "9": {
+            x: 462.3525,
+            name: "Belgium",
+            parent_id: "BE",
+            y: "408.666"
+        },
+        "10": {
+            name: "Netherland",
+            parent_id: "NL",
+            x: "462.352",
+            y: "366.666"
+        },
+        "11": {
+            name: "Austria",
+            parent_id: "AT"
+        }
+    },
+    legend: {
+        entries: []
+    }
+};
